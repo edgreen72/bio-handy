e9774d72f9a5243759d08e42af45c31ba03a7f89
Ed Green <ed@soe.ucsc.edu>
2016-06-11 16:31:51 -0700

    Added pss-bam.pl - mapDamage like thingy that uses Bio::DB::Bam

---
+++ pss-bam.pl
@@ -0,0 +1,105 @@
+#!/usr/bin/perl
+
+use Bio::DB::Sam;
+use Getopt::Std;
+use vars qw( $opt_f $opt_b $opt_r $opt_F $opt_R );
+use strict;
+
+my ( $sam, $feature, $ref, $matches, $query, $i, $rcref, $rcquery );
+my ( @ref, @query, @rcref, @rcquery );
+my (@SUBS, @RCSUBS );
+my @BASES = ('A', 'C', 'G', 'T' );
+my %RC = ('A' => 'T', 'C' => 'G', 'G' => 'C', 'T' => 'A',
+	  'a' => 't', 'c' => 'g', 'g' => 'c', 't' => 'a',
+	  'N' => 'N', 'n' => 'n', 'X' => 'x', '-' => '-');
+	  
+&init();
+
+my $sam = Bio::DB::Sam->new( -bam   => $opt_b,
+			     -fasta => $opt_f );
+
+foreach $feature ( $sam->features('match') ) {
+    unless( $feature->unmapped ) {
+	($ref, $matches, $query) = $feature->padded_alignment();
+	if ( $feature->strand == -1 ) {
+	    $ref = &revcom( $ref );
+	    $query = &revcom( $query );
+	}
+	@ref   = split( '', $ref );
+	@query = split( '', $query );
+	for( $i = 0; $i < $opt_r; $i++ ) {
+	    $SUBS[$i]->{$ref[$i]}->{$query[$i]}++;
+	}
+
+	### Now the other way
+	$rcref   = &revcom( $ref );
+	$rcquery = &revcom( $query );
+	@rcref = split( '', $rcref );
+	@rcquery = split( '', $rcquery );
+	for( $i = 0; $i < $opt_r; $i++ ) {
+	    $RCSUBS[$i]->{$rcref[$i]}->{$rcquery[$i]}++;
+	}
+    }
+}
+
+&output( '### Forward read substitution counts',
+	 \@SUBS );
+
+&output( "\n\n### Reverse read substitution counts",
+	 \@RCSUBS );
+
+0;
+
+sub output {
+    my $header_string = shift;
+    my $s_p = shift;
+    my ( $i, $ref_base, $query_base, $count );
+    print( "$header_string\n" );
+
+    for( $i = 0; $i < $opt_r; $i++ ) {
+	print( "$i" );
+	foreach $ref_base (@BASES) {
+	    foreach $query_base (@BASES) {
+		if ( $s_p->[$i]->{$ref_base}->{$query_base} ) {
+		    $count = $s_p->[$i]->{$ref_base}->{$query_base};
+		}
+		else {
+		    $count = 0;
+		}
+		printf(" $count");
+	    }
+	}
+	print( "\n" );
+    }
+}
+
+#foreach $target ($sam->seq_ids) {
+#    $segment = $sam->segment( -seq_id => $target );
+#    my $iterator = $segment->features( -iterator => 1 );
+#    while( my $align = $iterator->next_seq ) {
+#	print "yes";
+#    }
+#}
+
+sub revcom {
+    my $seq = shift;
+    my @seq = split( '', $seq );
+    my @rcseq = map { $RC{$_} } reverse @seq;
+
+    return join( '', @rcseq );
+}
+
+sub init {
+    my $r_DEF = 15;
+    getopts( 'f:b:r:' );
+    unless( -f $opt_f &&
+	    -f $opt_b ) {
+	print( "pss-bam.pl -f <fasta file> -b <bam file>\n" );
+	print( "           -r <region length; default = $r_DEF\n" );
+	print( "Tests the Bio::DB::Sam module\n" );
+	exit( 0 );
+    }
+    unless( defined( $opt_r ) ) {
+	$opt_r = $r_DEF;
+    }
+}
