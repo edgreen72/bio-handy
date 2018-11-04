0357f3d1f43b5216772decc64f6a5b3a618106ba
Ed Green <ed@soe.ucsc.edu>
2018-11-04 14:58:34 -0800

    fasta-genome-io.c & .h - C code for parsing fasta genome sequences

---
+++ fasta-genome-io.c
@@ -0,0 +1,213 @@
+#include "fasta-genome-io.h"
+
+/* Takes filename as argument
+   Returns true IFF filename ends in .gz
+   False otherwise */
+int is_gz( const char* fq_fn ) {
+  size_t fn_len;
+  fn_len = strlen( fq_fn );
+  if ( (fq_fn[fn_len-3] == '.') &&
+       (fq_fn[fn_len-2] == 'g') &&
+       (fq_fn[fn_len-1] == 'z') ) {
+    return 1;
+  }
+  return 0;
+}
+
+/* Takes filename
+   Returns Fa_Src*; NULL => problem or nothing there
+*/
+Fa_Src* init_fasta_src( const char fn[] ) {
+  Fa_Src* fa_source;
+  if ( fn == NULL ) {
+    return NULL;
+  }
+  fa_source = (Fa_Src*)malloc(sizeof( Fa_Src ));
+  strcpy( fa_source->fn, fn );
+  fq_source->n = 0;
+  fa_source->seq_buffer = (char*)malloc(sizeof(char)*MAX_SEQ_LEN);
+  fa_source->seq_buffer[0] = '\0';
+  if ( is_gz( fn ) ) {
+    fa_source->is_gz = 1;
+    fa_source->fagz = gzopen( fa_source->fn, "r" );
+    if ( fa_source->fagz == NULL ) {
+      free( fa_source->seq_buffer );
+      free( fa_source );
+      return NULL;
+    }
+  }
+  else {
+    fa_source->is_gz = 0;
+    fa_source->fafp = fileOpen( fa_source->fn, "r" );
+    if ( fa_source->fafp == NULL ) {
+      free( fa_source->seq_buffer );
+      free( fa_source );
+      return NULL;
+    }
+  }
+  return fa_source;
+}
+
+/* get_next_fa
+   Args: Fa_Src* fa_source - the source (uncompressed or gz) 
+                 of some fasta data
+   Returns: Seq* pointer to new sequence; NULL if there was
+            any problem, like EOF
+   Calls the right parser type (gz or regular) to read the next
+   record and updates the fq_source->n if a fastq record is read
+   correctly.
+*/
+Seq* get_next_fa( Fa_Src* fa_source, Genome* genome ) {
+  Seq* seq;
+  seq = (Seq*)malloc(sizeof(Seq));
+  if ( fa_source->is_gz ) {
+    seq = gzread_fasta( fa_source->fagz, seq,
+			fa_source->seq_buffer );
+
+  }
+  else {
+    seq = read_fasta( fa_source->fafp, seq,
+		      fa_source->seq_buffer );
+  }
+  if ( seq == NULL ) {
+    free( seq );
+  }
+  else {
+    fa_source->n++;
+    genome->seq[ genome->n_seq ] = seq;
+    genome->n_seqs++;
+  }
+  return seq;
+}
+
+/* Args: FILE* fafp
+         Seq* seq
+         char* seq_buffer
+   Returns: 0 - everything is copacetic
+          non-zero if EOF or other problem
+   Reads the next fastq sequence from a regular file handle
+*/
+int read_fasta( FILE* fafp, Seq* seq, char* seq_buffer ) {
+  char c;
+  size_t i = 0;
+  c = fgetc(fafp);
+  if ( c == '>' ) {
+    c = fgetc(fafp);
+    while ( !isspace(c) ) { // load up the ID
+      seq->id[i++] = c;
+      c = fgetc(fafp);
+    }
+    seq->id[i] = '\0';
+    while( c != '\n' ) {
+      c = fgetc(fafp);
+    }
+    i = 0;
+    while( (c != '>') &&
+           (c != EOF) &&
+           (i < MAX_SEQ_LEN) ) {
+      if ( isspace(c) ) {
+        ;
+      }
+      else {
+        c = toupper(c);
+        seq_buffer[i++] = c;
+      }
+      c = fgetc( fafp );
+    }
+    seq_buffer[i] = '\0';
+    ungetc(c, fp);
+  }
+  else {
+    if ( c == EOF ) {
+      return -1;
+    }
+  }
+  if ( i == MAX_SEQ_LEN ) {
+    fprintf( stderr, "%s is truncated to %d\n", id, MAX_SEQ_LEN );
+  }
+  /* Now, make space in seq for copying the sequence */
+  seq->seq = (char*)malloc(sizeof(char)*i);
+  strcpy( seq->seq, seq_buffer );
+  seq->len = i;
+  return i;
+}
+
+/* Args: gzFile gzfp
+         Seq* seq
+         char* seq_buffer
+   Returns: 0 - everything is copacetic
+          non-zero if EOF or other problem
+   Reads the next fastq sequence from a gzFile handle
+*/
+int gzread_fasta( gzFile gzfp, Seq* seq, char* seq_buffer ) {
+  char c;
+  size_t i = 0;
+  c = gzgetc(gzfp);
+  if ( c == '>' ) {
+    c = gzgetc(gzfp);
+    while ( !isspace(c) ) { // load up the ID
+      seq->id[i++] = c;
+      c = gzgetc(gzfp);
+    }
+    seq->id[i] = '\0';
+    while( c != '\n' ) {
+      c = gzgetc(gzfp);
+    }
+    i = 0;
+    while( (c != '>') &&
+           (c != EOF) &&
+           (i < MAX_SEQ_LEN) ) {
+      if ( isspace(c) ) {
+        ;
+      }
+      else {
+        c = toupper(c);
+        seq_buffer[i++] = c;
+      }
+      c = gzgetc( gzfp );
+    }
+    seq_buffer[i] = '\0';
+    gzungetc(c, gzfp);
+  }
+  else {
+    if ( c == EOF ) {
+      return -1;
+    }
+  }
+  if ( i == MAX_SEQ_LEN ) {
+    fprintf( stderr, "%s is truncated to %d\n", id, MAX_SEQ_LEN );
+  }
+  /* Now, make space in seq for copying the sequence */
+  seq->seq = (char*)malloc(sizeof(char)*i);
+  strcpy( seq->seq, seq_buffer );
+  seq->len = i;
+  return i;
+}
+
+Seq* find_seq( Genome* genome, const char id[] ) {
+  Seq* target;
+  strcpy( genome->dummy->id, id );
+  target = bsearch( &genome->dummy, genome->seqs, genome->n_seqs,
+		    sizeof( Seq* ), chr_cmp );
+  if ( target == NULL ) {
+    return NULL;
+  }
+  else {
+    return target;
+  }
+}
+
+int chr_cmp( const void *v1, const void *v2 ) {
+  Seq* c1p = (Seq*) v1;
+  Seq* c2p = (Seq*) v2;
+  return strcmp( c1p->id, c2p->id );
+}
+
+Genome* init_genome( void ) {
+  Genome* genome;
+  genome = (Genome*)malloc(sizeof( Genome ));
+  genome->seqs = (Seq**)malloc(sizeof(Seq*)*MAX_GENOME_SEQS);
+  genome->dummy = (Seq*)malloc(sizeof(Seq));
+  genome->n_seqs = 0;
+  return genome;
+}
---
+++ fasta-genome-io.h
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <limits.h>
+#include <zlib.h>
+#define MAX_FN_LEN (2047)
+#define MAX_ID_LEN (511)
+#define MAX_SEQ_LEN (536870911)
+#define MAX_GENOME_SEQS (1000000)
+
+/* Data structures */
+typedef struct seq {
+  char id[MAX_ID_LEN + 1];
+  char seq*;
+  size_t len;
+} Seq;
+
+typedef struct genome {
+  Seq** seqs;
+  Seq* dummy; // used for searching
+  size_t n_seqs;
+} Genome;
+
+typedef struct fa_src {
+  char fn[MAX_FN_LEN+1];
+  char* seq_buffer;
+  int is_gz;
+  gzFile fagz;
+  FILE* fafp;
+  size_t n;
+} Fa_Src;
+
+/* Function prototypes */
+Genome* init_genome( void );
+Fa_Src* init_fasta_src( const char fn[] );
+Seq* get_next_fa( Fa_Src* fa_source, Genome* genome );
+int read_fasta( FILE* fp, Seq* seq, char* seq_buffer );
+int gzread_fasta( gzFile gzfp, Seq* seq, char* seq_buffer );
+Seq* find_seq( Genome* genome, const char id[] );o
+int is_gz( const char* fn );
+FILE* fileOpen( const char* name, char access_mode[] );
+
+int chr_cmp( const void *v1, const void *v2 );
